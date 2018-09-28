19be01f8b270833fa2abc45026816303eefff618
yangjixianl <809266287@qq.com>
2018-09-28 03:02:49 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Sample App') - Laravel</title>
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
     @include('layouts._header')
 
     <div class="container">
+      @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
     </div>    
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@
 Route::get('/', 'StaticPagesController@home') -> name('home');
 Route::get('/help', 'StaticPagesController@help') -> name('help');
 Route::get('/about', 'StaticPagesController@about') -> name('about');
-Route::get('signup', 'UserController@create') -> name('signup');
-
+Route::get('signup', 'UsersController@create') -> name('signup');
+Route::resource('users', 'UsersController');
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
